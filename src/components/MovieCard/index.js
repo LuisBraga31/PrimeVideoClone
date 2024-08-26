8c8c3ad3dca4638ba47a1e2c08e786066e70f6cb
Luis Gustavo Braga <luisgustavobsilva@gmail.com>
2024-08-26 15:03:45 -0300

    Criando componente MovieCard

---
+++ src/components/MovieCard/index.js
@@ -0,0 +1,17 @@
+import { TouchableOpacity, Image, StyleSheet } from "react-native"
+
+export const MovieCard = (props) => {
+
+    return (
+        <TouchableOpacity>
+            <Image style={styles.img} source={props.movieUrl}/>
+        </TouchableOpacity>
+    )
+
+}
+
+const styles = StyleSheet.create({ 
+    img: {
+        marginLeft: 20,
+    }
+})
